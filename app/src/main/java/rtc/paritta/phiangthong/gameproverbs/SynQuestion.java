751e84929ab655cfafbac37a259aceb329170a52
auiouy <auiprt@gmail.com>
2016-11-30 20:55:51 +0700

    Updet Size Pic

--- app/src/main/java/rtc/paritta/phiangthong/gameproverbs/SynQuestion.java
+++ app/src/main/java/rtc/paritta/phiangthong/gameproverbs/SynQuestion.java
@@ -1,8 +1,45 @@
 package rtc.paritta.phiangthong.gameproverbs;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
 /**
- * Created by YoYoo on 11/22/2016.
+ * Created by Administrator on 22/11/2559.
  */
 
-public class SynQuestion {
-}
+public class SynQuestion extends AsyncTask<Void, Void, String>{
+
+    //Explicit
+    private static final String urlJSON = "http://swiftcodingthai.com/ary/get_question.php";
+    private Context context;
+
+    public SynQuestion(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        try {
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url(urlJSON).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
+
+        } catch (Exception e) {
+            Log.d("gameV1", "e doIn ==> " + e.toString());
+            return null;
+        }
+
+
+
+    }
+}   // Main Class
